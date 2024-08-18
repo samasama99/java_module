c617b38a93c87453ce184af85d876e48b6408a53
Oussama Rahmouni <orahmoun@e2r6p1.1337.ma>
2024-08-18 16:00:27 +0100

    making SimpleObject into a Record instead of using a class

--- day07/ex00/src/main/java/org/example/SimpleObject.java
+++ day07/ex00/src/main/java/org/example/SimpleObject.java
@@ -2,40 +2,11 @@ package org.example;
 
 import java.util.Map;
 
-final class SimpleObject {
-    private final SimpleClass simpleClass;
-    private final String objectName;
-    private final Object object;
-    private final Map<String, App.SimpleMethod> simpleMethods;
-    private final Map<String, App.SimpleField> simpleFields;
-
-    SimpleObject(SimpleClass simpleClass,
-                 String objectName,
-                 Object object,
-                 Map<String, App.SimpleMethod> simpleMethods,
-                 Map<String, App.SimpleField> simpleFields
-    ) {
-        this.simpleClass = simpleClass;
-        this.objectName = objectName;
-        this.object = object;
-        this.simpleMethods = simpleMethods;
-        this.simpleFields = simpleFields;
-    }
-
-    public SimpleClass simpleClass() {
-        return simpleClass;
-    }
-
-    public String objectName() {
-        return objectName;
-    }
-
-    public Map<String, App.SimpleMethod> simpleMethods() {
-        return simpleMethods;
-    }
-
-    public Map<String, App.SimpleField> simpleFields() {
-        return simpleFields;
-    }
-
+public record SimpleObject(
+        SimpleClass simpleClass,
+        String objectName,
+        Object object,
+        Map<String, App.SimpleMethod> simpleMethods,
+        Map<String, App.SimpleField> simpleFields
+) {
 }
